d5eb07a6bca0c559fd703f9ce41b8008a89881ec
SwastikVerma <108559653+SwastikVerma@users.noreply.github.com>
2023-02-11 18:23:19 +0530

    Swastik change

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,9 @@ router.get('/',(req,res)=>{
 });
 //dashboard page
 router.get('/dashboard',(req,res)=>res.render('dashboard'));
-
-//this is only for test----delete this afterwards
-router.get('/client',(req,res)=>{
-    res.render('client')
-})
-router.get('/client/pastIssues',(req,res)=>{
-    res.render('pastIssues')
-})
+//worker page
+router.get('/worker',(req,res)=>res.render('worker_index'));
+router.get('/worker_all',(req,res)=>res.render('all_works'));
+router.get('/worker_applications',(req,res)=>res.render('my_applications'));
 
 module.exports = router;
